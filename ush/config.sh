b152c335adeb2508274f41ba574f8f410e461ca0
blake-j-allen <blake.j.allen@noaa.gov>
2025-04-17 20:57:56 +0000

    fixed typo at begging of wflow template xml, minor change to config.sh

--- ush/config.sh
+++ ush/config.sh
@@ -1,7 +1,7 @@
 MACHINE="jet"
 version="v0.7.7"
 #RESERVATION="rrfsdet"
-EXPT_BASEDIR="/lfs5/BMC/wrfruc/bjallen/JEDI-AOD/RRFS_AOD_ParkFire_DAexp1/${version}"
+EXPT_BASEDIR="/lfs5/BMC/wrfruc/bjallen/JEDI-AOD/RRFS_ParkFire_ctrl_1day/${version}"
 EXPT_SUBDIR="RRFS_CONUS_3km"
 
 PREDEF_GRID_NAME=RRFS_CONUS_3km
@@ -35,8 +35,7 @@ USE_CLM="TRUE"
 DO_PARALLEL_PRDGEN="FALSE"
 DO_GSIDIAG_OFFLINE="FALSE"
 DO_UPDATE_BC="FALSE"
-DO_JEDI_GLM_DA="FALSE"
-DO_JEDI_AOD_DA="TRUE"
+
 
 ### NOTE: config_defaults.sh contains a lot of documentation for the options ###
 
