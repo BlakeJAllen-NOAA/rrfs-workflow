8e5626731e9982980c57edd1378cddae31a56cb2
blake-j-allen <blake.j.allen@noaa.gov>
2025-07-17 16:41:42 +0000

    New scripts for JEDI AOD DA

---
+++ jobs/JRRFS_fv3jedi_aod.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#### for reference, below block from 3dvar test driver with test call for diffusion BEC generation included;
+
+# Set the queueing options  (double hash to actually comment out instead of acting as slurm directives)
+##SBATCH --nodes=12 --ntasks-per-node=10 --exclusive
+##SBATCH -t 0:15:00
+##SBATCH -A zrtrr 
+##SBATCH --partition=xjet,kjet
+##SBATCH -q debug ##urgent #batch ##debug
+##SBATCH -J fv3jedi_bec 
+
+#set -x
+
+#module purge
+#module use /lfs5/BMC/wrfruc/bjallen/JEDI-AOD/RDASApp/modulefiles
+#module load RDAS/jet.intel
+
+
+#export OOPS_TRACE=1
+#export OOPS_DEBUG=1
+#export MAIN_TRACE=1
+#export MAIN_DEBUG=1
+
+#3Dvar
+#mkdir -p Data/analysis
+#mkdir -p Data/hofx
+#srun -n 120 fv3jedi_var.x 3dvar_rrfssd_aodda_smoke_diffusion_j01_iodathin.yaml
+
+#### end test driver block
+
+. ${GLOBAL_VAR_DEFNS_FP}
+. $USHdir/source_util_funcs.sh
+
+source ${MODULE_FILE}
+
+ulimit -s unlimited
+ulimit -a
+
+
+# don't do JEDI AOD on cold start
+hour=${ANL_TIME:8:2}
+if [[ ${hour} -eq "03" ]] || [[ ${hour} -eq "15" ]]; then
+  if [ ${FV3FCST_TYPE} == "spinup" ]; then
+    exit 0
+  fi
+fi
+
+# don't JEDI AOD if no IODA file exists for the current analysis time
+echo ${VIIRS_AOD_OBS_FN}
+if [[ ! -e ${VIIRS_AOD_OBS_FN} ]]; then
+   echo "No AOD IODA file exists for this time"
+   exit 0
+fi
+
+if [ ! -d ${WORK_DIR} ]; then
+   mkdir -p ${WORK_DIR}
+fi
+cd ${WORK_DIR}
+# clean up
+unlink INPUT
+rm logfile.000000.out
+
+FV3NAMELIST=${WORK_DIR}/GSL_fv3input_for_aod.nml
+sed "s/LX,LY/${LX},${LY}/1" < ${FIX_JEDIAOD}/${NML_TMP} > ${FV3NAMELIST}
+
+
+if [[ ${FV3FCST_TYPE} == "spinup" ]]; then
+  task_name_det="fcst_fv3lam_spinup"
+else
+  task_name_det="fcst_fv3lam"
+fi
+
+
+#task_name_ens="fcst_fv3lam_spinup"
+task_name_ens="fcst_fv3lam"
+
+echo "bkg_root: ${BKG_ROOT}"
+BKG_DIR=${BKG_ROOT}/${task_name_det}/INPUT
+
+if [ ! -d ${WORK_DIR}/LOG ]; then
+   mkdir -p ${WORK_DIR}/LOG
+fi
+
+#not entirely sure this matters?
+#ln -sf ${FIX_JEDIAOD} ./INPUT  # link grid info to work_dir
+ln -sf ${BKG_DIR} ./INPUT
+
+#apparently also need to link to 3dvar_c84 Data directory (also contains grid info)
+ln -sf /lfs5/BMC/wrfruc/bjallen/JEDI-AOD/3dvar_c84/Data ./Data
+
+set -x
+# Setup YAML file variables
+WINDOW_TIME_STR=`date -d "${ANL_TIME::8} ${ANL_TIME:8:4} -1 hour" +%Y-%m-%dT%H:%M:00Z`
+ANL_TIME_STR=`date -d "${ANL_TIME::8} ${ANL_TIME:8:4}" +%Y-%m-%dT%H:%M:00Z`
+YYYYMMDD_HHMM=`date -d "${ANL_TIME::8} ${ANL_TIME:8:4}" +%Y%m%d.%H%M`
+
+sed "s/WINDOW_TIME/${WINDOW_TIME_STR}/g" < ${FIX_JEDIAOD}/fv3jedi_3dvar_aod_smoke.yaml_template | \
+   sed "s+EXPT_ROOT+${FIX_JEDIAOD}+g" | \
+   sed "s+FV3NAMELIST+${FV3NAMELIST}+g" | \
+   sed "s/ANALYSIS_TIME/${ANL_TIME_STR}/g" | \
+   sed "s+BKG_DIR+${BKG_DIR}+g" | \
+   sed "s/YYYYMMDD.HHMMSS/${YYYYMMDD_HHMM}00/g" | \
+   sed "s/FV3FCST_TYPE/${task_name_det}/g" | \
+   sed "s+FV3JEDI_DIAG_FN+${WORK_DIR}/${FV3JEDI_DIAG_FN}+g" | \
+   sed "s+AOD_OBS_FN+${VIIRS_AOD_OBS_FN}+g" | \
+   sed "s+OUT_DIR+${WORK_DIR}+g" | \
+   sed "s/DESC/${FV3JEDI_AOD_DESC}/g" > ${WORK_DIR}/fv3jedi_3dvar_aod_smoke.yaml
+
+cat ${WORK_DIR}/fv3jedi_3dvar_aod_smoke.yaml
+srun --export=ALL ${FV3JEDI_BINDIR}/fv3jedi_var.x fv3jedi_3dvar_aod_smoke.yaml ${WORK_DIR}/LOG/LOG_VAR_AOD_SMOKE
---
+++ jobs/JRRFS_modify_ICs_aod.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+#There is also an exrrfs script like this one. Should this be a JRRFS or exrrfs script?
+
+#dynamics updates are commented out but not deleted below. Later on, especially upon adding ensemble DA,
+#more than just aerosol fields may need updated (nevermind, I'll include some of the dynamics updates).
+
+#Question: What about updates from other tasks, like conventional DA w/ GSI and the cloud analysis? 
+#Won't using the M1H (prior cycle) data below keep those updates from being included?
+
+#Perhaps I'll need to copy over every updated variabe from both the dynamics and physics files.
+#Was that what was being done for dbz and lightning DA?
+
+. ${GLOBAL_VAR_DEFNS_FP}
+. $USHdir/source_util_funcs.sh
+
+module load nco
+
+#below may not be needed for AOD, and doesn't quite work right for FED, either
+#we'll see if there is an error without it.
+#source $USHdir/run_convert_UV.sh
+
+#hack to keep JEDI-IC task from overwriting things while testing hofx and envar tasks
+#echo 'Exiting after convert_UV.sh with nonzero error code to halt workflow during testing'
+#echo 'Remove or comment out these lines near top of modify_ICs.sh to run normally'
+#exit 1
+
+# don't use JEDI AOD DA at cold start hour 
+#(guessing this is needed because the data files are different before the first forecast integration?)
+hour=${ATIME:8:2}
+if [[ ${hour} -eq "03" ]] || [[ ${hour} -eq "15" ]]; then
+  if [ ${CYCLE_TYPE} == "spinup" ]; then
+    exit 0
+  fi
+fi
+
+# don't JEDI AOD if no IODA file exists for the current analysis time
+echo ${VIIRS_AOD_OBS_FN}
+if [[ ! -e ${VIIRS_AOD_OBS_FN} ]]; then
+   echo "No AOD IODA file exists for this time"
+   exit 0
+fi
+
+set -x
+
+#not needed for AOD 
+#if [ ! -f ${JEDI_DIR}/convertstate/re-staggered_UV.fv_core.res.nc ]; then
+#  echo "Error: D-grid wind file missing"
+#  exit 1
+#fi
+
+cd ${JEDI_DIR}
+
+#Rename the y axis here because the jedi envar task returns the wrong name (no longer needed?)
+ncrename -d yaxis_1,yaxis_2 -v yaxis_1,yaxis_2 ${PREFIX}.fv_core.res.nc
+ncatted -a long_name,yaxis_2,o,c,yaxis_2 ${PREFIX}.fv_core.res.nc
+
+#shouldn't be needed for AOD
+#copy u,v from re-stagger file into lightning.fv_core.res.nc
+#ncks -v u,v convertstate/re-staggered_UV.fv_core.res.nc -O backup.re-stagger.fv_core.res.nc
+#ncks -A -v u,v backup.re-stagger.fv_core.res.nc ${PREFIX}.fv_core.res.nc
+
+#processing below here mostly from radar DA script
+yyyymmdd_hhmm=`date -d "${ATIME:0:8} ${ATIME:8:4}" +%Y%m%d.%H%M`
+dyn_file=${JEDI_DIR}/${PREFIX}.fv_core.res.nc 
+phy_file=${JEDI_DIR}/${PREFIX}.fv_tracer.res.nc
+phys_fields=(Time xaxis_1 yaxis_1 zaxis_1 sphum smoke dust coarsepm)
+dyn_fields=(Time xaxis_1 yaxis_2 zaxis_1 T delp ps phis)
+
+if [ -f ${JEDI_DIR}/fv_core.temp.nc ]; then
+   rm ${JEDI_DIR}/fv_core.temp.nc
+fi
+
+#maybe not needed for AOD?
+ncrename -v .w,W -v .DELP,delp -v .t,T ${dyn_file} ${JEDI_DIR}/fv_core.temp.nc
+err=$?
+if [ ${err} -ne 0 ]; then
+   echo "Error running ncrename on ${dyn_file}"
+   exit ${err}
+fi
+
+for p in ${phys_fields[@]}; do
+   ncatted -a checksum,${p},d,, -O ${phy_file}
+   err=$?
+   if [ ${err} -ne 0 ]; then
+      echo "Error running ncatted on ${phy_file}"
+      exit ${err}
+   fi
+done
+
+for d in ${dyn_fields[@]}; do
+   ncatted -a checksum,${d},d,, -O ${JEDI_DIR}/fv_core.temp.nc
+   err=$?
+   if [ ${err} -ne 0 ]; then
+      echo "Error running ncatted on ${JEDI_DIR}/fv_core.temp.nc"
+   fi
+done
+
+#Unclear if the file we want here should come from the previous hour
+#This is because the process_smoke tasks updte the smoke fields at the start of each cycle.
+#OTOH, the AOD DA acts on those updated smoke fields, and thus replacing the M1H fields
+#with the JEDI-updated fields should give the same result as  replacing the fields output by process_smoke
+#On the other other hand, this scripts exits at the beginning of spin-up (03Z and 15Z), and that is the only
+#time process_smoke_spinup alters the fields (to continuously cycle smoke from the prior prod cycle). process_smoke
+#has no need to update the model fields aside from those initial hours of spinup cycles.
+#Going to stick with modifying M1H data for now..
+#Question: What about updates from other tasks, like conventional DA w/ GSI and the cloud analysis? 
+
+#Won't using the M1H data keep those updates from being included?
+#actually, everything will be included so long as we copy everything that is updated over from the newly creted files.
+hour=${ATIME:8:2}
+if [ ${CYCLE_TYPE} == "prod" ]; then
+   DEST_ROOT=${DEST_DIR}/fcst_fv3lam/INPUT
+   if [[ ${hour} == "09" ]] || [[ ${hour} == "21" ]]; then
+      nwges_dir=${NWGES_ROOT_M1H}/fcst_fv3lam_spinup/RESTART
+   else
+      nwges_dir=${NWGES_ROOT_M1H}/fcst_fv3lam/RESTART
+   fi
+elif [ ${CYCLE_TYPE} == "spinup" ]; then
+   DEST_ROOT=${DEST_DIR}/fcst_fv3lam_spinup/INPUT
+   nwges_dir=${NWGES_ROOT_M1H}/fcst_fv3lam_spinup/RESTART
+else
+   echo "improper value for \$CYCLE_TYPE (must be either \"prod\" or \"spinup\""
+   exit 3
+fi
+
+#maybe not needed for AOD?
+# Apply updates to dynamics file
+# Unsure if the coordinate vars should be included here (or in the above lists), but this approach gets rid of all the float/double type warnings
+ncks -x -v T,delp,ps,phis ${nwges_dir}/${yyyymmdd_hhmm}00.fv_core.res.tile1.nc -O ${DEST_ROOT}/backup_core.nc
+err=$?
+if [ ${err} -ne 0 ]; then
+   echo "ncks exited with error code ${err}"
+   exit ${err}
+fi
+vlist=""
+for d in ${dyn_fields[@]}; do
+   vlist=${vlist}${d}","
+done
+ncks -v ${vlist::-1} ${JEDI_DIR}/fv_core.temp.nc -A ${DEST_ROOT}/backup_core.nc
+rm ${JEDI_DIR}/fv_core.temp.nc
+mv ${DEST_ROOT}/backup_core.nc ${DEST_ROOT}/fv_core.res.tile1.nc
+
+# Append variables to RRFS physics restart file (can't do this earlier because I need the $nwges_dir from 1 h prior from the above code first)
+vlist=""
+script=""
+for p in ${phys_fields[@]}; do
+   vlist=${vlist}${p}","
+   script=${script}"$p=float(${p});"
+done
+
+# Change variable types from "double" to "float"
+ncap2  -O -s "${script::-1}" ${phy_file} ${phy_file}
+
+#copy updated physics data to final tracer file
+ncks -A -v ${vlist::-1} ${phy_file} ${DEST_ROOT}/fv_tracer.res.tile1.nc
+
+if [ ${err} -ne 0 ]; then
+   echo "ncks exited with error code ${err}"
+   exit ${err}
+fi
